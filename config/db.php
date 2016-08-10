9cdf5223578b64138cfd5e82ba8993153aceac2b
Shakhzadov <k.shakhzadov@gmail.com>
2016-08-11 00:15:47 +0300

    changed config/db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=resource_registry',
     'username' => 'root',
-    'password' => '1slovn',
+    'password' => '7160722',
     'charset' => 'utf8',
 ];
 // Jenkins
